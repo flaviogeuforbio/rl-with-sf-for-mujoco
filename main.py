132771ea5c902568613776bbd89af530490f0e27
adrianoarceri <adrianoarceri@gmail.com>
2026-05-20 10:33:39 +0200

    Random stuff [Adriano Arceri]

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import math
+import numpy as np
+import torch
 
 print(math.log(2))
 print("hello world")
 print("my mouth got a little bit impasted.")
 
 print("I am Got.")
+for i in range(10):
+    print(i)
+
+print(np.array([1, 2, 3]))
+
+print(torch.tensor([1, 2, 3]))
